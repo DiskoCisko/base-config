eb9e704748d81513d421631a18cd21b88754a3a8
MIKHAIL NECHAEV <pollandjon@gmail.com>
2023-11-14 23:30:28 +0300

    separate config

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,77 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { merge } = require('webpack-merge');
 
-module.exports = (env) => {
-
-  console.log('Goal: ', env.goal); // 'local'
-  console.log('Production: ', env.production); // true
-
-  return {
-    entry: {
-      index: './src/index.js',
-      print: './src/print.js',
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        title: 'Output Management',
-      }),
+const commonConfig = { 
+  entry: {
+    index: './src/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
+  output: {
+    filename: '[name].[contenthash].main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        loader: 'babel-loader',
+      },
     ],
-    output: {
-      filename: '[name].[contenthash].main.js',
-      path: path.resolve(__dirname, 'dist'),
-      clean: true,
-      publicPath: '/',
-    },
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-      static: './dist',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
-        },
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
-        },
-        {
-          test: /\.(woff|woff2|eot|ttf|otf)$/i,
-          type: 'asset/resource',
-        },
-        {
-          test: /\.js$/,
-          include: path.resolve(__dirname, 'src'),
-          loader: 'babel-loader',
-        },
-      ],
-    },
-    optimization: {
-      runtimeChunk: 'single',
-      moduleIds: 'deterministic',
-      splitChunks: {
-        cacheGroups: {
-          vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name: 'vendors',
-            chunks: 'all',
-          },
+  }, 
+};
+
+const productionConfig = {
+  mode: 'production',
+  optimization: {
+    runtimeChunk: 'single',
+    moduleIds: 'deterministic',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
         },
       },
     },
-  }
+  },
 };
+
+const developmentConfig = { 
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+ };
+
+module.exports = (env) => {
+  switch(env.mode) {
+    case 'development':
+      return merge(commonConfig, developmentConfig);
+    case 'production':
+      return merge(commonConfig, productionConfig);
+    default:
+      throw new Error('No matching configuration was found!');
+  }
+}
